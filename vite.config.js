be883d6f93004975c9896bf5cc7b89e7939ca38b
moudev <rigobertomauriciomartinez@gmail.com>
2022-09-23 00:00:58 -0600

    build: add vite local server

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,12 @@ export default defineConfig({
   build: {
     lib: {
       name: 'OneClickTwitchMessage',  
-      entry: resolve(__dirname, 'src/firefox/run.js'),
+      entry: resolve(__dirname, 'src/firefox/one-click-twitch-message.js'),
       fileName: 'one-click-twitch-message'
     },
     outDir: "build/"
-  }
+  },
+  // https://vitejs.dev/config/shared-options.html#publicdir
+  // devServer will serve this directory with `serve` script: https://vitejs.dev/config/server-options.html
+  publicDir: resolve(__dirname, 'build/')
 })
