c338c4ce18332f3cec253dd208c0ae0b15aef221
moudev <rigobertomauriciomartinez@gmail.com>
2022-11-15 19:34:53 -0600

    fix(chat): update selector when the file is loaded
    the emotes weren't loaded because the selector was undefined
    when the file was loaded. For that reason the current chat
    was undefined. The solution was do the dom query selector
    when the function was called

--- src/twitch.js
+++ src/twitch.js
@@ -1,12 +1,12 @@
  // copy of: https://github.com/night/betterttv/blob/7.4.36/src/utils/twitch.js
-const TWITCH_SEND_BUTTON_CSS_SELECTOR = 'button[data-a-target="chat-send-button"]'
+const querySelector = (selector) => document.querySelector(selector)
 
-const CHAT_INPUT_NODE_OPTIONS = [
-  document.querySelector('textarea[data-a-target="chat-input"]'),
-  document.querySelector('div[data-a-target="chat-input"')
+const CHAT_INPUT_OPTIONS = [
+  'textarea[data-a-target="chat-input"]',
+  'div[data-a-target="chat-input"'
 ]
 
-const CHAT_CONTAINER = document.querySelector('section[data-test-selector="chat-room-component-layout"]')
+const CHAT_CONTAINER = 'section[data-test-selector="chat-room-component-layout"]'
 
 // NOTE: only works when the input is a textarea. It works in firefox developer edition
 // It doesn't work because cannot find the react instance in the element.
@@ -25,7 +25,7 @@ function getCurrentChat() {
   let currentChat;
   try {
     const node = searchReactParents(
-      getReactInstance(CHAT_CONTAINER),
+      getReactInstance(querySelector(CHAT_CONTAINER)),
       (n) => n.stateNode && n.stateNode.props && n.stateNode.props.onSendMessage
     );
     currentChat = node.stateNode;
@@ -38,7 +38,7 @@ function  getChatInputEditor(element = null) {
   let chatInputEditor;
   try {
     chatInputEditor = searchReactParents(
-      getReactInstance(element || CHAT_INPUT_NODE_OPTIONS[0] || CHAT_INPUT_NODE_OPTIONS[1]),
+      getReactInstance(element || querySelector(CHAT_INPUT_OPTIONS[0]) || querySelector(CHAT_INPUT_OPTIONS[1])),
       (n) => n.stateNode?.state?.slateEditor != null
     );
   } catch (_) {}
@@ -56,7 +56,7 @@ function getCurrentTwitchEmotes() {
 
   try {
     const node = searchReactParents(
-      getReactInstance(CHAT_CONTAINER),
+      getReactInstance(querySelector(CHAT_CONTAINER)),
       (n) => n.stateNode?.props?.emoteSetsData?.emoteMap,
       25
     );
@@ -104,57 +104,12 @@ function getChatInput(element = null) {
   let chatInput;
   try {
     chatInput = searchReactParents(
-      getReactInstance(element || CHAT_INPUT_NODE_OPTIONS[0] || CHAT_INPUT_NODE_OPTIONS[1]),
+      getReactInstance(element || querySelector(CHAT_INPUT_OPTIONS[0]) || querySelector(CHAT_INPUT_OPTIONS[1])),
       (n) => n.memoizedProps && n.memoizedProps.componentType != null && n.memoizedProps.value != null
     );
   } catch (_) {}
 
   return chatInput;
-}
-
-function setChatInputValue(text, shouldFocus = true) {
-  const element = CHAT_INPUT_NODE_OPTIONS[0] ? CHAT_INPUT_NODE_OPTIONS[0] : CHAT_INPUT_NODE_OPTIONS[1]
-
-  // deprecated
-  const {value: currentValue, selectionStart} = element;
-  if (currentValue != null) {
-    element.value = text;
-    element.dispatchEvent(new Event('input', {bubbles: true}));
-
-    const instance = getReactInstance(element);
-    if (instance) {
-      const props = instance.memoizedProps;
-      if (props && props.onChange) {
-        props.onChange({target: element});
-      }
-    }
-
-    const selectionEnd = selectionStart + text.length;
-    element.setSelectionRange(selectionEnd, selectionEnd);
-
-    if (shouldFocus) {
-      element.focus();
-    }
-
-    return;
-  }
-
-  const chatInput = this.getChatInput(element);
-  if (chatInput == null) {
-    return;
-  }
-
-  chatInput.memoizedProps.value = text;
-  chatInput.memoizedProps.setInputValue(text);
-  chatInput.memoizedProps.onValueUpdate(text);
-
-  if (shouldFocus) {
-    const chatInputEditor = this.getChatInputEditor(element);
-    if (chatInputEditor != null) {
-      chatInputEditor.focus();
-      chatInputEditor.setSelectionRange(text.length);
-    }
-  }
 }
 
 const getCurrentChannelInformation = () => {
